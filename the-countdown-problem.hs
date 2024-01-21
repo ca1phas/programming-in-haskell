2405410d3dd1d31d70dd77d89eb0b286dd67ae89
Casimir Chiong <casimirchiong@gmail.com>
2024-01-21 21:36:07 +0800

    the-countdown-problem: textbook solution

--- the-countdown-problem.hs
+++ the-countdown-problem.hs
@@ -1,58 +1,168 @@
-import Control.Monad (guard)
-import Data.List (permutations)
+import Data.List (sortBy)
 
-data Expr
-  = Val Int
-  | Add Expr Expr
-  | Sub Expr Expr
-  | Mul Expr Expr
-  | Div Expr Expr
+data Op = Add | Sub | Mul | Div
+
+ops :: [Op]
+ops = [Add, Sub, Mul, Div]
+
+instance Show Op where
+  show :: Op -> String
+  show Add = "+"
+  show Sub = "-"
+  show Mul = "*"
+  show Div = "/"
+
+valid :: Op -> Int -> Int -> Bool
+valid Sub x y = x > y
+valid Div x y = x `mod` y == 0
+valid _ _ _ = True
+
+valid' :: Op -> Int -> Int -> Bool
+valid' Sub x y = x > y
+valid' Add x y = x <= y
+valid' Mul x y = x <= y && x /= 1 && y /= 1
+valid' Div x y = x `mod` y == 0 && y /= 1
+
+apply :: Op -> Int -> Int -> Int
+apply Add x y = x + y
+apply Sub x y = x - y
+apply Mul x y = x * y
+apply Div x y = x `div` y
+
+data Expr = Val Int | App Op Expr Expr
 
 instance Show Expr where
   show :: Expr -> String
   show (Val n) = show n
-  show (Add l r) = show l ++ "+" ++ show r
-  show (Sub l r) = show l ++ "-" ++ show r
-  show (Mul (Val n) (Val m)) = show n ++ "*" ++ show m
-  show (Mul l r) = "(" ++ show l ++ ")*(" ++ show r ++ ")"
-  show (Div (Val n) (Val m)) = show n ++ "/" ++ show m
-  show (Div l r) = "(" ++ show l ++ ")/(" ++ show r ++ ")"
+  show (App o l r) = show' l ++ show o ++ show' r
+    where
+      show' (Val n) = show n
+      show' e = "(" ++ show e ++ ")"
 
-ops :: [Expr -> Expr -> Expr]
-ops = [Add, Sub, Mul, Div]
+values :: Expr -> [Int]
+values (Val n) = [n]
+values (App _ l r) = values l ++ values r
 
-comb :: [Int] -> [[Int]]
-comb [] = [[]]
-comb (x : xs) = cs ++ [x : c | c <- cs] where cs = comb xs
+eval :: Expr -> [Int]
+eval (Val n) = [n | n > 0]
+eval (App o l r) =
+  [ apply o x y
+    | x <- eval l,
+      y <- eval r,
+      valid o x y
+  ]
 
-perm :: [Int] -> [[Int]]
-perm xs = [p | c <- comb xs, p <- permutations c]
+subs :: [a] -> [[a]]
+subs [] = [[]]
+subs (x : xs) = yss ++ map (x :) yss where yss = subs xs
 
-split :: [Int] -> [([Int], [Int])]
-split xs = [splitAt n xs | n <- [1 .. length xs - 1]]
+interleave :: a -> [a] -> [[a]]
+interleave x [] = [[x]]
+interleave x (y : ys) = (x : y : ys) : map (y :) (interleave x ys)
 
-generate' :: [Int] -> [Expr]
-generate' [] = []
-generate' [x] = [Val x]
-generate' xs = [op el er | p <- perm xs, (ls, rs) <- split p, el <- generate' ls, er <- generate' rs, op <- ops]
+perms :: [a] -> [[a]]
+perms [] = [[]]
+perms (x : xs) = concatMap (interleave x) (perms xs)
 
-generate :: [Int] -> [Expr]
-generate xs = [Val x | x <- xs] ++ generate' xs
+choices :: [a] -> [[a]]
+choices = concatMap perms . subs
 
-eval :: Expr -> Maybe Int
-eval (Val n) = if n > 0 then Just n else Nothing
-eval (Add l r) = (+) <$> eval l <*> eval r
-eval (Mul l r) = (*) <$> eval l <*> eval r
-eval (Sub l r) = do
-  x <- eval l
-  y <- eval r
-  guard (x > y)
-  Just (x - y)
-eval (Div l r) = do
-  x <- eval l
-  y <- eval r
-  guard (x `mod` y == 0)
-  Just (x `div` y)
+solution :: Expr -> [Int] -> Int -> Bool
+solution e ns n = values e `elem` choices ns && eval e == [n]
 
-solutions :: Int -> [Int] -> [Expr]
-solutions t xs = [e | e <- generate xs, eval e == Just t]
+split :: [a] -> [([a], [a])]
+split [] = []
+split [_] = []
+split (x : xs) = ([x], xs) : [(x : ls, rs) | (ls, rs) <- split xs]
+
+exprs :: [Int] -> [Expr]
+exprs [] = []
+exprs [n] = [Val n]
+exprs ns =
+  [ e
+    | (ls, rs) <- split ns,
+      l <- exprs ls,
+      r <- exprs rs,
+      e <- combine l r
+  ]
+
+combine :: Expr -> Expr -> [Expr]
+combine l r = [App o l r | o <- ops]
+
+solutions :: [Int] -> Int -> [Expr]
+solutions ns n = [e | ns' <- choices ns, e <- exprs ns', eval e == [n]]
+
+type Result = (Expr, Int)
+
+results :: [Int] -> [Result]
+results [] = []
+results [n] = [(Val n, n) | n > 0]
+results ns =
+  [ res
+    | (ls, rs) <- split ns,
+      lx <- results ls,
+      ry <- results rs,
+      res <- combine' lx ry
+  ]
+
+combine' :: Result -> Result -> [Result]
+combine' (l, x) (r, y) = [(App o l r, apply o x y) | o <- ops, valid' o x y]
+
+solutions' :: [Int] -> Int -> [Expr]
+solutions' ns n = [e | ns' <- choices ns, (e, m) <- results ns', m == n]
+
+-- Chapter 9
+-- Q1
+choices' :: [a] -> [[a]]
+choices' xs = [ps | ss <- subs xs, ps <- perms ss]
+
+-- Q2
+removeone :: (Eq a) => a -> [a] -> [a]
+removeone x [] = []
+removeone x (y : ys)
+  | x == y = ys
+  | otherwise = y : removeone x ys
+
+isChoice :: (Eq a) => [a] -> [a] -> Bool
+isChoice [] _ = True
+isChoice _ [] = False
+isChoice (x : xs) ys = x `elem` ys && isChoice xs (removeone x ys)
+
+-- Q4
+-- total = 33,665,406
+total :: Int
+total = length [e | cs <- choices [1, 3, 7, 10, 25, 50], e <- exprs cs]
+
+-- stotal = 245,644
+stotal :: Int
+stotal = length [i | cs <- choices [1, 3, 7, 10, 25, 50], e <- exprs cs, i <- eval e]
+
+-- Q6
+-- b
+solutions'' :: [Int] -> Int -> [Expr]
+solutions'' ns n
+  | null ss = solutions'' ns (n - 1) ++ solutions'' ns (n + 1)
+  | otherwise = ss
+  where
+    ss = solutions' ns n
+
+-- c
+eops :: Expr -> [Op]
+eops (Val n) = []
+eops (App o l r) = o : eops l ++ eops r
+
+sortGT :: (Ord a1, Ord a2) => (a1, a2, c1) -> (a1, a2, c2) -> Ordering
+sortGT (a, b, _) (c, d, _)
+  | a < c = LT
+  | a > c = GT
+  | otherwise = compare b d
+
+osolutions :: [Int] -> Int -> [Expr]
+osolutions ns n = [s | (_, _, s) <- sortBy sortGT ss]
+  where
+    ss =
+      [ (length $ values e, length $ eops e, e)
+        | ns' <- choices ns,
+          (e, m) <- results ns',
+          m == n
+      ]
